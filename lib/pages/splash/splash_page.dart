8be454da22603236c823e486763aece958eae08e
Phùng Tiến Thành <phungtienthanh@anvui.vn>
2020-08-10 23:43:25 +0700

    Update code cho base.

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ncckios/base/color.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -9,7 +10,28 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          width: MediaQuery.of(context).size.width / 3 * 2,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            color: AppColor.primaryDarkColor,
+            padding: const EdgeInsets.all(4),
+            child: Center(
+              child: Text(
+                'Developed by AN VUI',
+                style: Theme.of(context).textTheme.bodyText2.copyWith(color: AppColor.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
